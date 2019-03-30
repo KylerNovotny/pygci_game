9eebc1726af39784274f0daed6b252fb7189639e
Kyler Novotny <kylernovotny@email.arizona.edu>
2019-03-30 14:33:50 -0700

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -3,5 +3,5 @@ mysql = {'host': 'gamedata.cwrmsieis5r4.us-west-2.rds.amazonaws.com',
          'user': 'kyler',
          'passwd': 'koolio5589',
          'db': 'gamedata'}
-webhost = {'host':str(urllib.request.urlopen("http://169.254.169.254/latest/meta-data/public-ipv4").read())
+webhost = {'host':str(urllib.request.urlopen("http://169.254.169.254/latest/meta-data/public-ipv4").read().decode('utf-8')
            }
