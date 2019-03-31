386599b76f312f87bf7c87744d784e53c3712256
Kyler Novotny <kylernovotny@email.arizona.edu>
2019-03-30 17:21:46 -0700

    Update initGame.py
    slight update

--- initGame.py
+++ initGame.py
@@ -13,19 +13,17 @@ def check_validity():
     #TODO: add check for empty form
     playerName = form["playerName"].value
     if playerName.isalpha() and len(playerName) < 100:
-        
-    else:
-        raise FormError("Player names can only alphanumeric characters and must be of length <100.")
-        return
-
-    conn = MySQLdb.connect(host=login.mysql['host'],
+        conn = MySQLdb.connect(host=login.mysql['host'],
                            user=login.mysql['user'],
                            passwd=login.mysql['passwd'],
                            db=login.mysql['db'])
-    c=conn.cursor()
-    c.execute("""INSERT INTO TABLE (playerName,choicepath,items,winstatus,dead)
+        c=conn.cursor()
+        c.execute("""INSERT INTO TABLE (playerName,choicepath,items,winstatus,dead)
               VALUES (%s,'','',0,0)"""%(playerName))
-    return cursor.lastrowid;
+        return cursor.lastrowid;
+    else:
+        raise FormError("Player names can only alphanumeric characters and must be of length <100.")
+        return
 
 try:
     print("Content-Type: text/html")
