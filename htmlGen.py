c7614f4ba2e483f5a63f0e25dd6193818422666f
Kyler Novotny <kylernovotny@email.arizona.edu>
2019-04-01 17:02:18 -0700

    Update htmlGen.py

--- htmlGen.py
+++ htmlGen.py
@@ -95,9 +95,9 @@ width: “50”
         print("""<p>: %s</p>"""%(nextChoice[1] not in items))
         #make sure that the required items for that choice are held
         #and item is not already in inventory
-        if(req in items) and (nextChoice[1] not in items):
+        if(req == None) or (req in items):
             #if the next choice is an item pickup
-            if len(nextChoice)==3:
+            if len(nextChoice)==3 && (nextChoice[1] not in items):
                 choiceStr += """<td>%s<button type="submit" name=item value="%s"></button></td>"""% (nextChoice[0],nextChoice[1])
             #if the next choice is a choice
             else:
