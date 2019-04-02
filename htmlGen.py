601bfd19832f153db31403f326baee14090a1cf2
Kyler Novotny <kylernovotny@email.arizona.edu>
2019-04-01 17:05:32 -0700

    Update htmlGen.py

--- htmlGen.py
+++ htmlGen.py
@@ -90,9 +90,6 @@ width: “50”
         nextChoice = get_avail_choices(choicepath+str(num));
         desc = nextChoice[0]
         req = nextChoice[2]
-        print("""<p>: %s</p>"""% nextChoice)
-        print("""<p>: %s</p>"""%(req in items))
-        print("""<p>: %s</p>"""%(nextChoice[1] not in items))
         #make sure that the required items for that choice are held
         #and item is not already in inventory
         if(req == None) or (req in items):
