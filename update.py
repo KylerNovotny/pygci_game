5b144a0fee5abee66f2be013bdfa30727a5916ba
Kyler Novotny <kylernovotny@email.arizona.edu>
2019-04-01 19:35:14 -0700

    Update update.py
    still trying to find error

--- update.py
+++ update.py
@@ -12,43 +12,43 @@ from common import FormError
 def check_validity():
     form = cgi.FieldStorage()
     
-    print(1)
-    if("item" not in form and "choice" not in form) or ("gameId" not in form):
-        raise FormError("Form does not contain a decision.")
-        return
+    
+#    if("item" not in form and "choice" not in form) or ("gameId" not in form):
+#        raise FormError("Form does not contain a decision.")
+#        return
     
     gameId = form["gameId"].value
     
-    if("item" in form):
-        item = form["item"].value
-    else:
-        choice = form["choice"].value
-        
-    conn  = MySQLdb.connect(host=login.mysql['host'],
-                           user=login.mysql['user'],
-                           passwd=login.mysql['passwd'],
-                           db=login.mysql['db'])
-    c=conn.cursor()
-    query = """SELECT * FROM gamedata WHERE id=%s""" % gameId
-    c.execute(query)
-    if(c.rowcount != 1):
-        raise FormError("Invalid game ID")
-    gameInfo = c.fetchAll()[0]
-    choicepath = gameInfo[2]
-    items = gameInfo[3].split(",")
-
-    
-    if("item" in form):
-        items.append(item)
-        query = """UPDATE gamedata SET items=%s WHERE id=%s"""%(items,gameId)
-    else:
-        query = """UPDATE gamedata SET choicepath=%s WHERE id=%s"""%(choicepath+choice,gameId)
-
-    c.execute(query)
-        
-    conn.commit()
-    c.close()
-    conn.close()
+#    if("item" in form):
+#        item = form["item"].value
+#    else:
+#        choice = form["choice"].value
+#        
+#    conn  = MySQLdb.connect(host=login.mysql['host'],
+#                           user=login.mysql['user'],
+#                           passwd=login.mysql['passwd'],
+#                           db=login.mysql['db'])
+#    c=conn.cursor()
+#    query = """SELECT * FROM gamedata WHERE id=%s""" % gameId
+#    c.execute(query)
+#    if(c.rowcount != 1):
+#        raise FormError("Invalid game ID")
+#    gameInfo = c.fetchAll()[0]
+#    choicepath = gameInfo[2]
+#    items = gameInfo[3].split(",")
+#
+#    
+#    if("item" in form):
+#        items.append(item)
+#        query = """UPDATE gamedata SET items=%s WHERE id=%s"""%(items,gameId)
+#    else:
+#        query = """UPDATE gamedata SET choicepath=%s WHERE id=%s"""%(choicepath+choice,gameId)
+#
+#    c.execute(query)
+#        
+#    conn.commit()
+#    c.close()
+#    conn.close()
     return gameId
 
 try:
