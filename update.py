d32f79987b7a16bc99dfc2bfc17f2e569a8a9bb8
Kyler Novotny <kylernovotny@email.arizona.edu>
2019-04-01 20:00:44 -0700

    Update update.py

--- update.py
+++ update.py
@@ -17,8 +17,8 @@ def check_validity():
         #raise FormError("Form does not contain a decision.")
         #return
     
-    #gameId = form["gameId"].value
-    gameId=21
+    gameId = form["gameId"].value
+    
     if("item" in form):
         item = form["item"].value
     else:
